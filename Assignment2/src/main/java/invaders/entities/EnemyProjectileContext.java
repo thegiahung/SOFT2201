38e11782a240e03255ad3fb5c3b443e0a26a3e93
The Hung Pham <tpha8248@uni.sydney.edu.au>
2023-09-22 19:53:15 +1000

    delete redundant ProjectileContext class

--- Assignment2/src/main/java/invaders/entities/EnemyProjectileContext.java
+++
@@ -1,9 +0,0 @@
-package invaders.entities;
-
-public class EnemyProjectileContext {
-    private Projectile projectile;
-
-    public static Projectile context(Projectile projectile) {
-        return projectile;
-    }
-}
